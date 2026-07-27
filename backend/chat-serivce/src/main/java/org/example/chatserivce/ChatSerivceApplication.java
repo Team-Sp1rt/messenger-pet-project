6a57659889ecb73e50c70369127725e532beb0fa
1ns_demon <chelovefick@gmail.com>
2026-07-27 19:50:57 +0300

    Initialising project

---
+++ backend/chat-serivce/src/main/java/org/example/chatserivce/ChatSerivceApplication.java
@@ -0,0 +1,13 @@
+package org.example.chatserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatSerivceApplication.class, args);
+    }
+
+}
