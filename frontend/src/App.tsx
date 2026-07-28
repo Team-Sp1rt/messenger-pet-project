9b09a30246cfb09683c8ed8a831fed61e7e0cdde
Andrew <97516045+Exam-Play@users.noreply.github.com>
2026-07-28 19:54:41 +0300

    [DEVOPS] Added Dockerfile for the backend + initial backend-frontend integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import {useEffect, useState} from "react";
+
 function App() {
-  return (
-    <div>
-      <p>Hello, world!!!</p>
-    </div>
-  )
+    const [helloWorld, setHelloWorld] = useState(':(');
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/hello')
+            .then(res => res.text())
+            .then(data => setHelloWorld(data))
+    }, [])
+
+    return (
+        <div>
+            <p>{helloWorld}</p>
+        </div>
+    )
 }
 
 export default App;
