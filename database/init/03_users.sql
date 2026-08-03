3d28f39562d6e0768994724813b8b19ab9f680f3
1ns_demon <chelovefick@gmail.com>
2026-08-03 20:39:22 +0300

    [DB/INITIALISATION] changing making some columns UNIQUE

--- database/init/03_users.sql
+++ database/init/03_users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id BIGINT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     bio VARCHAR(50),
     birthday DATE,
     FOREIGN KEY (id) REFERENCES authorisation(id)
