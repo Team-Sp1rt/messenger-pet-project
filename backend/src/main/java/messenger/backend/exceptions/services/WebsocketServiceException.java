69752d43682696685b214d5c3b99ca5104fad046
sonychello <sofapozneeva@gmail.com>
2026-08-24 12:30:31 +0500

    [BACKEND/SERVICE/WEBSOCKET] Fixed the Websocket exception and service. Added validation to the WebSocket controller.

--- backend/src/main/java/messenger/backend/exceptions/services/WebsocketServiceException.java
+++ backend/src/main/java/messenger/backend/exceptions/services/WebsocketServiceException.java
@@ -4,17 +4,11 @@ import messenger.backend.dtos.WebSocketErrorCode;
 
 public class WebsocketServiceException extends RuntimeException {
   private final WebSocketErrorCode code;
-  private final String message;
 
   public WebsocketServiceException(WebSocketErrorCode code,
                                    String message) {
+      super(message);
       this.code = code;
-      this.message = message;
-  }
-
-  @Override
-    public String getMessage() {
-      return message;
   }
 
   public WebSocketErrorCode getCode() {
