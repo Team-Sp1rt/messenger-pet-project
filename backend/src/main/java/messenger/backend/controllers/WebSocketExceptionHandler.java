8f80a98c67e83d8dc58ab08b39cd7bde49c64f9a
Karmanova Alyona <karaliona2006@gmail.com>
2026-08-26 12:17:01 +0300

    [BACKEND/WEBSOCKET] Updated STOMP authentication and error handling

--- backend/src/main/java/messenger/backend/controllers/WebSocketExceptionHandler.java
+++ backend/src/main/java/messenger/backend/controllers/WebSocketExceptionHandler.java
@@ -2,6 +2,7 @@ package messenger.backend.controllers;
 
 import messenger.backend.dtos.WebSocketError;
 import messenger.backend.dtos.WebSocketErrorCode;
+import messenger.backend.exceptions.services.WebsocketServiceException;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.messaging.simp.annotation.SendToUser;
@@ -17,6 +18,14 @@ public class WebSocketExceptionHandler {
                 WebSocketErrorCode.MESSAGE_INVALID_CONTENT,
                 "Message must contain from 1 to 300 characters"
         );
+    }
+
+    @MessageExceptionHandler(WebsocketServiceException.class)
+    @SendToUser(value = "/queue/errors", broadcast = false)
+    public WebSocketError handleServiceException(
+            WebsocketServiceException exception
+    ) {
+        return new WebSocketError(exception.getCode(), exception.getMessage());
     }
 
 
