1bbd7bdd5e56abcbb8012779a4fcae33c4ae84e4
1ns_demon <chelovefick@gmail.com>
2026-08-30 15:44:03 +0300

    [BACKEND/REPOS/USER] added getUserSummariesByIDs

--- backend/src/main/java/messenger/backend/repositories/UserRepository.java
+++ backend/src/main/java/messenger/backend/repositories/UserRepository.java
@@ -11,6 +11,7 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class UserRepository {
@@ -61,7 +62,7 @@ public class UserRepository {
         }
     }
 
-    public UserSummary getUserSummaryByID(Long id)throws SQLException, NoSuchUserException {
+    public UserSummary getUserSummaryByID(Long id) throws SQLException, NoSuchUserException {
         String sql = """
             SELECT id, username FROM users
             WHERE id = ?;
@@ -80,6 +81,34 @@ public class UserRepository {
             }
         } finally {
             DataSourceUtils.releaseConnection(connection, dataSource);
+        }
+    }
+
+    public List<UserSummary> getUserSummariesByIDs(Set<Long> ids) throws SQLException {
+        String sql = """
+                SELECT id, username FROM users
+                WHERE id IN (""" + createEnoughQuestions(ids.size()) + ");";
+
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            setAllVariables(preparedStatement, ids);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return mapResultSetToUserSummariesList(resultSet);
+            }
+        } finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+        }
+    }
+
+    private String createEnoughQuestions(int length) {
+        return "?, ".repeat(Math.max(0, length - 1)) + '?';
+    }
+
+    private void setAllVariables(PreparedStatement preparedStatement, Set<Long> ids) throws SQLException {
+        int i = 1;
+        for (Long id : ids) {
+            preparedStatement.setLong(i++, id);
         }
     }
 
